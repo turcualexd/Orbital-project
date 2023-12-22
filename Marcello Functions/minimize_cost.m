a0b79822f33583a6463ebf83c0394adc6df5c88e
PaneeVino <107176433+PaneeVino@users.noreply.github.com>
2023-12-22 19:30:25 +0100

    Last Updates we made

--- Marcello Functions/minimize_cost.m
+++ Marcello Functions/minimize_cost.m
@@ -1,82 +1,47 @@
-function [dv, dep, TOF_1, TOF_2] = minimize_cost(date_dep, date_arr)
+function [dv_min, dep_min, fb_min, arr_min] = minimize_cost(dep_vec, TOF1_vec,TOF2_vec)
 
-mjd2000_dep_min = date2mjd2000(date_dep);
+dv_min = 1e6;
 
-mjd2000_arr_max = date2mjd2000(date_arr);
+dep_min = NaN;
 
-dtprec = (mjd2000_arr_max - mjd2000_dep_min) / 2;
+fb_min = NaN;
 
-dtofprec = (2*365-1*365) / 2;
+arr_min = NaN;
 
-mjd2000_deptilde = mjd2000_dep_min + dtprec;
+fun = @(x) total_cost(x);
 
-TOF1tilde = 1 * 365 + dtofprec;
+for i = 1 : length(dep_vec)
 
-TOF2tilde = 1 * 365 + dtofprec;
+    dep_date = dep_vec(i);
 
-dv = 1e10;
+    for j = 1 : length(TOF1_vec)
 
-dvprec = dv;
+        TOF1 = TOF1_vec(j);
 
-toll = 1e-6;
+        for k = 1 : length(TOF2_vec)
 
-while 1
+            TOF2 = TOF2_vec(k);
 
-    dt = dtprec / 2;
+            x0 = [dep_date; TOF1; TOF2];
 
-    dtof = dtofprec / 2;
+            [x, dv] = fminunc(fun, x0);
 
-    for mjd2000_dep = mjd2000_deptilde - dtprec : dt : mjd2000_deptilde + dtprec
+            if dv < dv_min
 
-        for TOF1 = TOF1tilde - dtofprec : dtof : TOF1tilde + dtofprec
+                dep_min = x(1);
 
-            for TOF2 = TOF2tilde - dtprec : dt : TOF2tilde + dtprec
+                fb_min = dep_min + x(2);
 
-                [dv1, ~, V_m] = cost_lambert(mjd2000_dep, mjd2000_dep + TOF1, 4, 3, 0);
+                arr_min = fb_min + x(3);
 
-                [dv2, ~, V_p] = cost_lambert_2(mjd2000_dep + TOF1, mjd2000_dep + TOF1 + TOF2, 3, 65, 0 );
+                dv_min = dv;
 
-                dv3 = cost_gravity_assist(mjd2000_dep + TOF1, 3, V_m, V_p);
-
-                dvtot = dv1 + dv2 + dv3;
-
-                dt_tot = mjd2000_dep + TOF1 + TOF2;
-
-                condition = (dvtot<dv).*(dt_tot<mjd2000_arr_max);
-
-                if condition == 1
-
-                    dv = dvtot;
-
-                    dep = mjd2000_dep; 
-
-                    TOF_1 = TOF1;
-
-                    TOF_2 = TOF2;
-
-                end
             end
 
         end
 
     end
 
-    dtprec = dt;
-
-    dtofprec = dtof;
-
-    mjd2000_deptilde = dep;
-
-    TOF1tilde = TOF_1;
-
-    TOF2tilde = TOF_2;
-
-    if abs(dv-dvprec) < toll
-
-        break
-
-    end
-
-    dvprec = dv;
+end
 
 end
