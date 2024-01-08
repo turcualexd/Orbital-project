8625eb955b0712200db93cad2d9c1f1dfb0e0a9d
danieleP14 <115355495+danieleP14@users.noreply.github.com>
2024-01-08 20:11:13 +0100

    algorithm description

--- Marcello Functions/brute_force.m
+++ Marcello Functions/brute_force.m
@@ -21,18 +21,20 @@ function [dv_min, dep_date, fb_date, arr_date] = brute_force(T_dep, TOF1, TOF2)
 %
 
 dv_min = 1e10;
-
+wait = waitbar(0, 'Minimum search... (0%)');
 for i = 1 : length(T_dep)
-    for j = 1 : length(TOF1)
+   waitbar(i/length(T_dep), wait, sprintf('Minimum search... (%g%%)', i/length(T_dep)* 100));
+    
+   for j = 1 : length(TOF1)
+        
+        T_fb  = T_dep(i) + TOF1(j);
+        [dv1, ~, V_m] = cost_lambert(T_dep(i), T_fb, 4, 3, 0);
+        
         for k = 1 : length(TOF2)
             
-            T_fb  = T_dep(i) + TOF1(j);
             T_arr = T_fb     + TOF2(k);
-
-            [dv1, ~, V_m] = cost_lambert(T_dep(i), T_fb, 4, 3, 0);
             [dv2, V_p, ~] = cost_lambert_2(T_fb, T_arr, 3, 65, 0);
-           
-            [dv3, rp] = cost_gravity_assist(T_fb, 3, V_m, V_p);
+            [dv3, rp]     = cost_gravity_assist(T_fb, 3, V_m, V_p);
             
             dvtot = dv1 + dv2 + dv3;
 
@@ -47,3 +49,5 @@ for i = 1 : length(T_dep)
         end
     end
 end
+
+close(wait);
