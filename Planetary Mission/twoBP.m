f0ed79876e8b24454d9e0e7b0e0cdbe834ac5495
Tamim <81691845+TamimHR@users.noreply.github.com>
2024-01-10 15:20:32 +0100

    Update twoBP.m

--- Planetary Mission/twoBP.m
+++ Planetary Mission/twoBP.m
@@ -1,6 +1,35 @@
 function ds = twoBP(state,a_dist,mu,fr)
 
-% Function for the ODE of the Two Body Problem, both for the perturbed and unperturbed 
+% Function used to model the ODE of the Two Body Problem, both for the
+% perturbed and unperturbed cases.
+%
+% INPUTS
+% state [6x1]   Initial state. Can be a Cartesian state vector in
+%               the format [r;v] where r is the position and
+%               v is the initial velocity. [L] and [L/T].
+%               Can also be a vector with the initial
+%               Keplerian elements [Semi-major axis, eccentricity,
+%               inclination, RAAN, Argument of perigee, True anomaly].
+%               The units are [[L], [-], [rad], [rad], [rad], [rad]].
+%               If frame=='XYZ', it must be [r;v] vector.
+%               If frame=='TNH' or frame=='RSW', it must be Keplerian
+%               elements vector.
+% a_dist [3x1]  Disturbing acceleration in the respective reference
+%               frame. [L/T^2].
+% mu [1]        Gravitational parameter of the central body [L^3/T^2]
+% fr [char]     Reference frame in which the two body problem ODE will be 
+%               computed. Must be:
+%                   - 'XYZ': Earth centered Inertial.
+%                   - 'RSW': Radial transversal out-of-plane.
+%                   - 'TNH': Tangential normal out-of-plane.
+%
+% OUTPUT
+% ds [6x1]      Derivatives of the state vector with respect to time in
+%               it's respective reference frame (same as input).
+%               If in XYZ frame, the units are:
+%               [L/T];[L/T];[L/T];[L/T^2];[L/T^2];[L/T^2]
+%               If in RSW or TNH frame, units are:
+%               [L/T];[T^-1];[rad/T];[rad/T];[rad/T];[rad/T]
 
 if strcmp(fr,'XYZ')
     % slices the variables
